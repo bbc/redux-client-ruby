13ac3d58c66a5e855e2b58d0aacb86b3582e8414
Matt Haynes <matt@matthaynes.net>
2011-10-11 22:26:37 +0100

    Add user XML parsing

--- lib/bbc_redux/user.rb
+++ lib/bbc_redux/user.rb
@@ -2,6 +2,21 @@ module BBC
   module Redux
     class User
 
+      def self.from_xml(xml_string)
+
+        xml = Nokogiri::XML(xml_string)
+
+        self.new({
+          :id         => xml.xpath("/response/user/id").first.text.to_i,
+          :username   => xml.xpath("/response/user/username").first.text,
+          :first_name => xml.xpath("/response/user/first_name").first.text,
+          :last_name  => xml.xpath("/response/user/last_name").first.text,
+          :email      => xml.xpath("/response/user/email").first.text,
+          :session    => Session.new(xml.xpath("/response/user/token").first.text)
+        })
+
+      end
+
       attr_reader :id, :username, :first_name, :last_name, :email, :session
 
       def initialize(attributes)
--- spec/bbc_redux/user_spec.rb
+++ spec/bbc_redux/user_spec.rb
@@ -4,7 +4,25 @@ describe BBC::Redux::User do
 
   describe ".from_xml" do
     it "should build an object from an XML response" do
-      pending
+      xml = %Q{<?xml version="1.0" encoding="UTF-8" ?>
+      <response>
+        <user>
+          <token>some-token</token>
+          <id>1234</id>
+          <username>testuser</username>
+          <email>test.user@example.com</email>
+          <first_name>Test</first_name>
+          <last_name>User</last_name>
+        </user>
+      </response>
+      }
+      xml_user = BBC::Redux::User.from_xml(xml)
+      xml_user.id.should             == 1234
+      xml_user.username.should       == "testuser"
+      xml_user.email.should          == "test.user@example.com"
+      xml_user.first_name.should     == "Test"
+      xml_user.last_name.should      == "User"
+      xml_user.session.token.should  == "some-token"
     end
   end
 
